0999834eeab1e6f24a86610c95d61910de282651
jesus2102 <jesusperezmarin@gmail.com>
2018-08-09 14:53:16 +0100

    dropdown populated with regions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const Munros = require('./models/munros.js');
 const MunrosList = require('./views/munros_list.js');
+const SelectedView = require('./views/selected_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
@@ -7,6 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector(`.munros-list`);
   const munrosList = new MunrosList(container);
   munrosList.bindingEvents();
+
+  const dropdown = document.querySelector(`.region-dropdown`);
+  const selectedView = new SelectedView(dropdown);
+  selectedView.bindingEvents();
 
 
   const munros = new Munros();
--- src/views/selected_view.js
+++ src/views/selected_view.js
@@ -5,7 +5,19 @@ const SelectedView = function (list) {
 }
 
 SelectedView.prototype.bindingEvents = function () {
-  
+  PubSub.subscribe('Munros:all-regions-ready', (evt) => {
+    const result = evt.detail;
+    this.populate(result);
+  });
+};
+
+SelectedView.prototype.populate = function (allData) {
+  allData.forEach((data, index) => {
+    const option = document.createElement(`option`);
+    option.textContent = data.name;
+    option.value = index;
+    this.list.appendChild(option);
+  })
 };
 
 
