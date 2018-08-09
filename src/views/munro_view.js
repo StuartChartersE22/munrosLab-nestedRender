a3886c0a50e906758be2fb67230f16b9c7384af7
jesus2102 <jesusperezmarin@gmail.com>
2018-08-09 13:45:50 +0100

    MVP done

--- src/views/munro_view.js
+++ src/views/munro_view.js
@@ -5,15 +5,35 @@ const MunroView = function (container, munro) {
 
 MunroView.prototype.render = function () {
   addMunroName(this.munro.name, this.container);
-  // addNameMeaning(this.munro.meaning, this.container);
-  // addHeight(this.munro.height, this.container);
+  addListOfDetails(this.munro, this.container);
+
 };
 
 function addMunroName(name, container) {
-  const munroName = document.createElement(`h2`);
+  const munroName = document.createElement(`h3`);
   munroName.classList.add(`munro-name`);
   munroName.textContent = name;
   container.appendChild(munroName);
+}
+
+function addListOfDetails(munro, container) {
+  const list = document.createElement(`ul`);
+  addNameMeaning(munro.meaning, list);
+  addHeight(munro.height, list);
+  container.appendChild(list);
+}
+
+
+function addNameMeaning (meaning, list) {
+  const munroMeaning = document.createElement(`li`);
+  munroMeaning.textContent = `Meaning: ` + meaning;
+  list.appendChild(munroMeaning);
+}
+
+function addHeight(height, list) {
+  const munroHeight = document.createElement(`li`);
+  munroHeight.textContent = `Height: ` + height;
+  list.appendChild(munroHeight);
 }
 
 module.exports = MunroView;
